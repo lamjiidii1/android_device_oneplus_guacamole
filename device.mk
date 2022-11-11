80dbd337fa770099fa082644f1d6ec79f1e1a48d
jhonboy121 <alfredmathew05@gmail.com>
2022-11-12 00:03:59 +0100

    guacamole: Overlay: AlertSlider -> OplusHwManager
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- device.mk
+++ device.mk
@@ -28,13 +28,13 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
-    AlertSliderOverlay \
     FlamingoOPlus7ProFrameworks \
     FlamingoOPlus7ProSystemUI \
     OPlus7ProFrameworks \
     OPlus7ProSettingsProvider \
     OPlus7ProSettings \
-    OPlus7ProSystemUI
+    OPlus7ProSystemUI \
+    OplusHwManagerOverlay
 
 # Shipping API
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
